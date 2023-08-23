3b362cd3c55a2405248829b006a52e22ac220f05
Allen Chen <alchen@drwholdings.com>
2023-08-23 08:08:01 -0500

    remove neogit

--- plugins/user.lua
+++ plugins/user.lua
@@ -123,14 +123,4 @@ return {
       }
     end,
   },
-  {
-    "NeogitOrg/neogit",
-    dependencies = {
-      "nvim-lua/plenary.nvim", -- required
-      "nvim-telescope/telescope.nvim", -- optional
-      "sindrets/diffview.nvim", -- optional
-    },
-    config = true,
-    lazy = false,
-  },
 }
