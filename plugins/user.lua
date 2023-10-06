ee5d47b95aee01d35c4c4cc7cebe1c0f1e78aec6
Allen Chen <alchen@drwholdings.com>
2023-10-06 11:59:03 -0500

    add color themes

--- plugins/user.lua
+++ plugins/user.lua
@@ -123,4 +123,64 @@ return {
       }
     end,
   },
+  {
+    "rebelot/kanagawa.nvim",
+    as = "kanagawa",
+    lazy = false,
+    config = function()
+      require("kanagawa").setup {
+        compile = false, -- enable compiling the colorscheme
+        undercurl = true, -- enable undercurls
+        commentStyle = { italic = true },
+        functionStyle = {},
+        keywordStyle = { italic = true },
+        statementStyle = { bold = true },
+        typeStyle = {},
+        transparent = false, -- do not set background color
+        dimInactive = false, -- dim inactive window `:h hl-NormalNC`
+        terminalColors = true, -- define vim.g.terminal_color_{0,17}
+        colors = { -- add/modify theme and palette colors
+          palette = {},
+          theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+        },
+        overrides = function(colors) -- add/modify highlights
+          return {}
+        end,
+        theme = "wave", -- Load "wave" theme when 'background' option is not set
+        background = { -- map the value of 'background' option to a theme
+          dark = "wave", -- try "dragon" !
+          light = "lotus",
+        },
+      }
+    end,
+  },
+  {
+    "Alexis12119/nightly.nvim",
+    as = "nightly",
+    lazy = false,
+    config = function()
+      require("nightly").setup {
+        transparent = false,
+        styles = {
+          comments = { italic = true },
+          functions = { italic = false },
+          variables = { italic = false },
+          keywords = { italic = false },
+        },
+        highlights = {},
+      }
+    end,
+  },
+  {
+    "Shatur/neovim-ayu",
+    as = "ayu",
+    lazy = false,
+    config = function()
+      require("lualine").setup {
+        options = {
+          theme = "ayu",
+        },
+      }
+    end,
+  },
 }
